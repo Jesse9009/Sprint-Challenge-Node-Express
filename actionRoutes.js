e8448715b937cef3a1ff0f07c200effe8101ecc5
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-12-08 11:38:07 -0600

    created route files and have the files ready for endpoints to be added

---
+++ actionRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const actions = require('./data/helpers/actionModel');
+
+const router = express.Router();
+
+// Endpoints
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('yarn started');
+const express = require('express');
+const logger = require('morgan');
+
+const server = express();
+
+const port = 4050;
+
+const actionRoutes = require('./actionRoutes');
+const projectRoutes = require('./projectRoutes');
+
+// Middleware
+server.use(express.json());
+server.use(logger('dev'));
+
+// Routes
+server.use('/api/actions', actionRoutes);
+server.use('/api/projects', projectRoutes);
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
