f74a1cade8d1a0a545f5285b97da81de0b426647
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-12-08 12:14:13 -0600

    added GET and POST methods to the action endpoint

--- actionRoutes.js
+++ actionRoutes.js
@@ -5,5 +5,37 @@ const actions = require('./data/helpers/actionModel');
 const router = express.Router();
 
 // Endpoints
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  actions
+    .get(id)
+    .then(action => {
+      res.json(action);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Could not get action data' });
+    });
+});
+
+router.post('/', (req, res) => {
+  const action = req.body;
+  if (
+    action.project_id &&
+    action.description &&
+    action.notes &&
+    (action.completed === true || action.completed === false)
+  ) {
+    actions
+      .insert(action)
+      .then(action => {
+        res.json(action);
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'Could not add action data' });
+      });
+  } else {
+    res.status(400).json({ message: 'Please provide all required data' });
+  }
+});
 
 module.exports = router;
